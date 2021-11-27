56839adbcff6206993e8905f02be72ba051a5cb6
Blackdog7777 <69210104+Blackdog7777@users.noreply.github.com>
2021-11-27 20:54:51 +0300

    main

---
+++ main.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+
+class PhoneContact:
+    def __init__(self):
+        self.connection = sqlite3.connect("base.db")
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS PhoneList(
+                            name TEXT,
+                            contactNumber INT,
+                            note TEXT
+                       )""")
+
+        self.connection.commit()
+
+    def view(self):
+        outset = []
+        for i in self.cursor.execute("select * form PhoneList"):
+            outset += {f"{i[0]}": [{"number": f"{str(i[1])}"}, {"note": f"{str(i[2])}"}]}
+        print(outset)
+
+
+class PhoneBook:
+    def __init__(self):
+        self.loc_connection = sqlite3.connect("base.db")
+        self.loc_cursor = self.loc_connection.cursor()
+        self.contactNumbers = []
+        for i in self.loc_cursor.execute("SELECT contactNumber FROM PhoneList"):
+            self.contactNumbers.append(str(i)[1:-2])
+
+    def getContactByName(self, name: str):
+        numbers = []
+        for i in self.loc_cursor.execute(f"SELECT contactNumber FROM PhoneList WHERE name = '{name}'"):
+            numbers.append(str(i)[1:-2])
+
+    def addContact(self, name: str, contactNumber: int, note: str):
+        self.loc_cursor.execute(f"INSERT INTO PhoneList VALUES('{name}', {contactNumber}, '{note}')")
+        self.loc_connection.commit()
+
+
+PhoneContact = PhoneContact()
+PhoneBook = PhoneBook()
+
+PhoneContact
+
+PhoneBook.addContact('abcd', 77777777777, 'abcd')
+PhoneBook.getContactByName('abcd')
